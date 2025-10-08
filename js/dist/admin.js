19eb5541bd00a357dc16c4e45f830b097309c8d8
Edge <dcc@monika.love>
2025-10-08 15:07:08 +0800

    Fixed rare 404 while autosaving

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -1,2 +1,177 @@
-(()=>{var e={n:t=>{var s=t&&t.__esModule?()=>t.default:()=>t;return e.d(s,{a:s}),s},d:(t,s)=>{for(var r in s)e.o(s,r)&&!e.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:s[r]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t)};(()=>{"use strict";const t=flarum.core.compat["admin/app"];var s=e.n(t);const r=flarum.core.compat["common/extend"];s().initializers.add("fof-drafts",(function(){s().extensionData.for("fof-drafts").registerSetting({setting:"fof-drafts.enable_scheduled_drafts",label:s().translator.trans("fof-drafts.admin.settings.enable_scheduled_drafts"),type:"boolean"}).registerSetting({setting:"fof-drafts.schedule_on_one_server",label:s().translator.trans("fof-drafts.admin.settings.schedule_on_one_server"),type:"boolean"}).registerSetting({setting:"fof-drafts.store_log_output",label:s().translator.trans("fof-drafts.admin.settings.schedule_log_output"),type:"boolean"}).registerPermission({icon:"fas fa-edit",label:s().translator.trans("fof-drafts.admin.permissions.start"),permission:"user.saveDrafts"},"start").registerPermission({icon:"fas fa-calendar-plus",label:s().translator.trans("fof-drafts.admin.permissions.schedule"),permission:"user.scheduleDrafts"},"start"),(0,r.extend)(s(),"getRequiredPermissions",(function(e,t){"user.scheduleDrafts"===t&&e.push("user.saveDrafts")}))}))})(),module.exports={}})();
+/******/ (() => { // webpackBootstrap
+/******/ 	// runtime can't be in strict mode because a global variable is assign and maybe created.
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/admin/index.ts":
+/*!****************************!*\
+  !*** ./src/admin/index.ts ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var flarum_admin_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! flarum/admin/app */ "flarum/admin/app");
+/* harmony import */ var flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_admin_app__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flarum/common/extend */ "flarum/common/extend");
+/* harmony import */ var flarum_common_extend__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__);
+/*
+ *
+ *  This file is part of fof/drafts.
+ *
+ *  Copyright (c) 2019 FriendsOfFlarum.
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ */
+
+
+
+flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().initializers.add('fof-drafts', function () {
+  flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().extensionData["for"]('fof-drafts').registerSetting({
+    setting: 'fof-drafts.enable_scheduled_drafts',
+    label: flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().translator.trans('fof-drafts.admin.settings.enable_scheduled_drafts'),
+    type: 'boolean'
+  }).registerSetting({
+    setting: 'fof-drafts.schedule_on_one_server',
+    label: flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().translator.trans('fof-drafts.admin.settings.schedule_on_one_server'),
+    type: 'boolean'
+  }).registerSetting({
+    setting: 'fof-drafts.store_log_output',
+    label: flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().translator.trans('fof-drafts.admin.settings.schedule_log_output'),
+    type: 'boolean'
+  }).registerPermission({
+    icon: 'fas fa-edit',
+    label: flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().translator.trans('fof-drafts.admin.permissions.start'),
+    permission: 'user.saveDrafts'
+  }, 'start').registerPermission({
+    icon: 'fas fa-calendar-plus',
+    label: flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default().translator.trans('fof-drafts.admin.permissions.schedule'),
+    permission: 'user.scheduleDrafts'
+  }, 'start');
+  (0,flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__.extend)((flarum_admin_app__WEBPACK_IMPORTED_MODULE_0___default()), 'getRequiredPermissions', function (required, permission) {
+    if (permission === 'user.scheduleDrafts') {
+      required.push('user.saveDrafts');
+    }
+  });
+});
+
+/***/ }),
+
+/***/ "flarum/admin/app":
+/*!**************************************************!*\
+  !*** external "flarum.core.compat['admin/app']" ***!
+  \**************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = flarum.core.compat['admin/app'];
+
+/***/ }),
+
+/***/ "flarum/common/extend":
+/*!******************************************************!*\
+  !*** external "flarum.core.compat['common/extend']" ***!
+  \******************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = flarum.core.compat['common/extend'];
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
+(() => {
+"use strict";
+/*!******************!*\
+  !*** ./admin.js ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _src_admin__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/admin */ "./src/admin/index.ts");
+/*
+ *
+ *  This file is part of fof/drafts.
+ *
+ *  Copyright (c) 2019 FriendsOfFlarum.
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ */
+
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
 //# sourceMappingURL=admin.js.map
